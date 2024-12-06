5064846b64e3cbac5c53b8b6ff6bae612a52e772
jnbdz <jn@yaloub.com>
2024-12-06 11:57:45 -0500

    Finally got the tab and content to load and function properly.

--- browser/frontend/assets/ux.js
+++ browser/frontend/assets/ux.js
@@ -1,14 +1,16 @@
-$(document).ready(function() {
-    // This initializes the tabs
-    $("#tabs").tabs({
-        activate: function(event, ui) {
-            // Remove 'pf-m-current' from all PatternFly tabs and panels
-            $(".pf-v6-c-tabs__item").removeClass("pf-m-current");
-            //$(".pf-v6-c-tabs__panel").removeClass("pf-m-current");
+function tabSystem() {
+    return {
+        activeTab: 'eql', // Default tab
 
-            // Add 'pf-m-current' to the selected tab and its associated panel
-            ui.newTab.addClass("pf-m-current");
-            ui.newPanel.addClass("pf-m-current");
+        init() {
+            this.setTab(this.activeTab);
+        },
+
+        setTab(tab) {
+            this.activeTab = tab;
+
+            // Move the content from the hidden tab to the main content area
+            //const tabEls = document.getElementsByClassName(`tab_${tab}`);
         }
-    });
-});
+    };
+}
